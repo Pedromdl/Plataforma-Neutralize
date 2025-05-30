34438d2ffff6b2895ea5d607a809b3f6eee15518
camilaalmeida-receitas <pedromartinstests@gmail.com>
2025-05-30 10:23:04 -0300

    Config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://plataforma-neutralize.onrender.com';
 
 
-// https://plataforma-neutralize.onrender.com
 // 
+// http://127.0.0.1:8000
